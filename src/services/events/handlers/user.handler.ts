611209107d1c4f83904817a298c2556c5c8d0edc
이현종 <ehdclr@gractor.com>
2024-12-23 16:55:30 +0900

    로그 삭제

--- src/services/events/handlers/user.handler.ts
+++ src/services/events/handlers/user.handler.ts
@@ -11,12 +11,11 @@ export const handleUserSignIn = async (data: userType.UserSignInAudit) => {
     const { user_id, email, name, timestamp, company_id } = data.payload;
 
     const auditEvent = {
-      topic : "link.event.user.signin.audit",
+      topic : "audit.user.signin",
       payload : { user_id, email, name , timestamp, company_id },
       action : "signIn",
       message : `${name}님이 로그인 하셨습니다.`
     }
-    console.log(auditEvent);
     await publishEvent(auditEvent.topic, auditEvent);
     
     Logger.info('사용자 로그인 로그 저장 성공', {  data });
